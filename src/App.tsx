2c24bcb650983d96225b94277da073f29c667d61
seon-mikim <tjsalsun@gmail.com>
2023-06-02 20:29:00 +0900

    conf: 프로젝트 초기 설정

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+
+
+function App() {
+  return <div>안녕</div>;
+}
+
+export default App;
---
+++ src/instance/AxiosInstance.ts
@@ -0,0 +1,16 @@
+import axios from "axios"
+
+const createInstance = () => {
+   const instance = axios.create({
+        baseURL: process.env.SERVER_URL,
+        timeout:3000,
+        headers: {
+            'Content-Type' : 'application/json',
+        }
+    
+    })
+
+    return instance
+
+}
+export const instance = createInstance()
