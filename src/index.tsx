150fcfc5aee0cf0492f23ae1d5ae67c4d031d09d
seon-mikim <tjsalsun@gmail.com>
2023-06-06 16:14:13 +0900

    feat: 라우팅을 위한 프로바이더 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { RouterProvider } from 'react-router';
+import Router from './router/Router';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={Router}/>
   </React.StrictMode>
 );
 
