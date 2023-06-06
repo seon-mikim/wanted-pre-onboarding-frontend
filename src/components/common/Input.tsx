0207bb71079baf4391dce7ec64b537074a07c930
seon-mikim <tjsalsun@gmail.com>
2023-06-06 19:20:18 +0900

    feat: Input Component 생성 및 조건부 랜더링 처리

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { useLocation } from 'react-router'
 
 export default function Input() {
+  const location = useLocation()
   return (
-    <div>Input</div>
+    <div>
+      { location.pathname === '/signup' || location.pathname === '/login' ?
+       <form>
+        <input type="text" />
+        <input type="password" />
+       </form>: <input type="text" />
+      }
+    </div>
   )
 }
