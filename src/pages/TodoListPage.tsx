70616499daa7c9253611fb4b85c08a2ceb84f962
seon-mikim <tjsalsun@gmail.com>
2023-06-06 19:24:21 +0900

    feat: TodoListPage에 Input Component Import 처리

--- src/pages/TodoListPage.tsx
+++ src/pages/TodoListPage.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import Input from '../components/common/Input'
 
 export default function TodoListPage() {
   return (
-    <div>TodoListPage</div>
+    <div>
+      <Input/>
+    </div>
   )
 }
