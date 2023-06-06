4d6ebc287caa6aa92e95be9dc67ae014d9bb93a0
seon-mikim <tjsalsun@gmail.com>
2023-06-06 16:36:51 +0900

    feat: hearder 컨포넌트 import 처리

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
 
 function App() {
   return(
           <div>
-              <Outlet/>
+            <Header/>
+            <Outlet/>
           </div>
     );
 }
