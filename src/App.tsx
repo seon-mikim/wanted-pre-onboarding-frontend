f86f8772a5180c203848e231cd41e3672a57e228
seon-mikim <tjsalsun@gmail.com>
2023-06-09 17:09:11 +0900

    refactor: 불필요한 코드 변경 및 라우터 임폴트 추가
    - 가독성과 이해하기 쉽게 코드 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,7 @@
-import { Outlet, useLocation } from 'react-router-dom';
-import Header from './components/Header';
-import Button from './components/common/Button';
+import Router from './router/Router'
 
 function App() {
-  const location = useLocation();
-
-  return(
-          <div>
-            <Header/>
-            <Outlet/>
-            { location.pathname !== '/todo'&&
-              <div>
-                { location.pathname !== '/signup' &&
-                    <Button title='sign up' color='blue' size='16px'/>
-                }
-                { location.pathname !== '/login' &&
-                    <Button title='login' color='blue' size='16px'/>
-                }
-              </div>
-            }
-          </div>
-    );
+  return Router
 }
 
-export default App;
+export default App
