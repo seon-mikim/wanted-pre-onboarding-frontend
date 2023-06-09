4f5460f13cfa9bc3b2bbdaee4541125fe0b3b78c
seon-mikim <tjsalsun@gmail.com>
2023-06-09 17:22:44 +0900

    feat: Link 컨포넌트 생성및 기능 구현
    - 해당 링크 컨포넌트는 페이지에서 내려오는
      프롭스 pathName에 따라 네비게이트 이동처리 됨

---
+++ src/components/common/link/Link.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useLocation } from 'react-router-dom'
+
+interface LinkProps {
+  pathName: string
+  onNavigate: (pathName: string) => void
+}
+
+export default function Link({ pathName, onNavigate }: LinkProps) {
+  const location = useLocation()
+
+  const linkComment = location.pathname === '/signin' || location.pathname === '/' 
+      ? '아직 회원이 아니시라면 회원가입 해주세요!!'
+      : '회원이라면 로그인해주세요!!'
+
+  const handleClick = () => {
+    if (location.pathname !== pathName) {
+      onNavigate(pathName)
+    } else if (pathName === '/') {
+      onNavigate('/signup')
+    }
+  }
+  return <div onClick={handleClick}>{linkComment}</div>
+}
