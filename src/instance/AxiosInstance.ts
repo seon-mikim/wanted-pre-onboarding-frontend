ad2f2131e890cd0c60c12bc7575badbb4bd53f9e
seon-mikim <tjsalsun@gmail.com>
2023-06-09 17:16:25 +0900

    feat: 엑시오스 인스턴스에서 인터셉터 리퀘스트시
    - 액세스토큰 있을시 todo 작성 가능하게 처리

--- src/instance/AxiosInstance.ts
+++ src/instance/AxiosInstance.ts
@@ -1,13 +1,18 @@
-import axios from "axios"
+import axios from 'axios'
 
 const createInstance = () => {
-   const instance = axios.create({
-        baseURL: process.env.SERVER_URL,
-        timeout:3000,
-        headers: {
-            'Content-Type' : 'application/json',
-        }
-    })
-    return instance
+  const instance = axios.create({
+    baseURL: process.env.REACT_APP_SERVICE_URL,
+    timeout: 3000,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+  instance.interceptors.request.use((request) => {
+    const getAccessToken = localStorage.getItem('accessToken')
+    if (getAccessToken) request.headers['Authorization'] = `Bearer ${getAccessToken}`
+    return request
+  })
+  return instance
 }
 export const instance = createInstance()
