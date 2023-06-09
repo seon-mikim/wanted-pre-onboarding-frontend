c5b30fea48c44c88abfb8bbddf1112388f1a745f
seon-mikim <tjsalsun@gmail.com>
2023-06-09 17:18:48 +0900

    feat: Link 컨포넌트 생성
    - 페이지에서 내려 오는 프롭스 조건에 따라 네이게이트되게 구현

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react'
+import Button from '../components/common/button/Button'
+import {  useNavigate } from 'react-router-dom'
+import Link from '../components/common/link/Link'
+
+export default function MainPage() {
+  const navigate = useNavigate()
+  const getAccessToken = localStorage.getItem('accessToken')
+  useEffect(()=> {
+    if(getAccessToken) {
+      navigate('/todo')
+     }
+  },[])
+
+  const clickHandle = (title: string) => {
+    navigate(title)
+  }
+
+  const onNavigate = (pathName:string) => {
+    navigate(pathName)
+ }
+
+  return (
+    <div>
+      <div>
+        <span>오늘 할일을 적고 싶을 땐...</span>
+      </div>
+      <div>
+        <Button title="/signin" clickHandle={clickHandle} />
+        <div>
+        <Link pathName='/' onNavigate={onNavigate}/>
+        </div>
+      </div>
+    </div>
+  )
+}
