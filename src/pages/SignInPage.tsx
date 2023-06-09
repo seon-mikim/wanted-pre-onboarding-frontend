e2bb617efd592ac7fa897aa18bf952489690906e
seon-mikim <tjsalsun@gmail.com>
2023-06-09 18:02:50 +0900

    feat: 해당 페이지에서 하위 컨포넌트로 프롭스  전달
    - 필요 함수 구현

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,10 +1,49 @@
-import React from 'react'
-import Input from '../components/common/Input'
+import { useEffect, useState } from 'react'
+import Input from '../components/common/input/Input'
+import Link from '../components/common/link/Link'
+import { useLocation, useNavigate } from 'react-router'
+import { postSignIn } from '../service/Api'
 
 export default function SignInPage() {
+  const [userInput, setUserInput] = useState({ email: '', password: '' })
+  const location = useLocation()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const getAccessToken = localStorage.getItem('accessToken')
+    if (getAccessToken) navigate('/todo')
+  }, [])
+
+  const formHandle = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    const res = await postSignIn(userInput)
+    console.log(res)
+    const { status, data } = res
+    setUserInput({ email: '', password: '' })
+    if (status === 200) {
+      return navigate('/todo')
+    }
+
+    if (status === 401) {
+      return alert(data.message)
+    }
+  }
+
+  const onChangeHandle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target
+    setUserInput({ ...userInput, [name]: value })
+  }
+
+  const onNavigate = (pathName: string) => {
+    navigate(pathName)
+  }
+
   return (
     <div>
-      <Input/>
+      <div style={{ fontWeight: 'bold', fontSize: '20px' }}>로그인</div>
+      <Input formHandle={formHandle} onChangeHandle={onChangeHandle} pathName={location.pathname} />
+      <Link pathName="/signup" onNavigate={onNavigate} />
     </div>
   )
 }
