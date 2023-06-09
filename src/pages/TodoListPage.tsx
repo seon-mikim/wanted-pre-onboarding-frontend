4d1093554db6ab7df4ff9437b593eeab0899125e
seon-mikim <tjsalsun@gmail.com>
2023-06-09 17:29:53 +0900

    feat: TodoListPage에 api 함수
    - 하위 컨포넌트에 프롭스 전달 처리

--- src/pages/TodoListPage.tsx
+++ src/pages/TodoListPage.tsx
@@ -1,10 +1,60 @@
-import React from 'react'
-import Input from '../components/common/Input'
+import React, { ChangeEvent, FormEvent, useEffect, useState } from 'react'
+import Input from '../components/common/input/Input'
+import { useLocation, useNavigate } from 'react-router'
+import { deleteTodo, getTodos, postTodo, updateTodo } from '../service/Api'
+import { Todo, UpdataTopoRequest } from '../interface/todo'
+import CardList from '../components/cardList/CardList'
 
 export default function TodoListPage() {
+  const [todoInput, setTodoInput] = useState('')
+  const [todos, setTodos] = useState<Todo[]>([])
+  const location = useLocation()
+  const navigate = useNavigate()
+  const pathName = location.pathname
+
+  useEffect(() => {
+    const getAccessToken = localStorage.getItem('accessToken')
+    if (!getAccessToken) navigate('/signin')
+  }, [])
+
+  useEffect(() => {
+    setTodosByResponse()
+  }, [])
+
+  const setTodosByResponse = async () => {
+    const data = await getTodos()
+    setTodos(data)
+  }
+
+  const formHandle = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    console.log(todoInput)
+    await postTodo(todoInput)
+    setTodoInput('')
+    await setTodosByResponse()
+  }
+
+  const onChangeHandle = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value
+    setTodoInput(value)
+  }
+
+  const onDelete = async (todoId: Todo['id']) => {
+    await deleteTodo(todoId)
+    await setTodosByResponse()
+  }
+
+  const onUpdate = async ({ todo, todoId, isCompleted }: UpdataTopoRequest) => {
+    await updateTodo({ todo, isCompleted, todoId })
+    await setTodosByResponse()
+  }
+
   return (
     <div>
-      <Input/>
+      <div style={{ marginBottom: '20px' }}>
+        <Input formHandle={formHandle} onChangeHandle={onChangeHandle} todoInput={todoInput} pathName={pathName} />
+      </div>
+      <div>{<CardList todos={todos} onDelete={onDelete} onUpdate={onUpdate} />}</div>
     </div>
   )
 }
