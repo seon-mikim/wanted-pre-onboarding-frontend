503dd0060f58b1373ec7543c166abadb7df03d10
seon-mikim <tjsalsun@gmail.com>
2023-06-09 17:08:23 +0900

    fix: 불필요한 import 삭제

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import { RouterProvider } from 'react-router';
 import Router from './router/Router';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
