4b0aa2db8542a73d957f40e37153a3844ba55a7f
seon-mikim <tjsalsun@gmail.com>
2023-06-10 21:25:50 +0900

    design: 인라인 스타일 적용
    - 스타일 컨포넌트 적용 전 인라인 스타일 적용

--- src/components/cardList/CardList.tsx
+++ src/components/cardList/CardList.tsx
@@ -2,17 +2,14 @@ import React from 'react'
 import { CardListProps } from '../../interface/todo'
 import Card from '../card/Card'
 
-
-
 export default function CardList({ todos, onDelete, onUpdate }: CardListProps) {
-   
   return (
-    <div style={{width:'100%'}}>
-   
+    <div style={{ width: '80%', marginLeft:'20px' }}>
+     
         {todos.map((todo) => (
-          <Card todo={todo} key={todo.id}  onDelete={onDelete} onUpdate={onUpdate} />
+          <Card todo={todo} key={todo.id} onDelete={onDelete} onUpdate={onUpdate} />
         ))}
-
+   
     </div>
   )
 }
