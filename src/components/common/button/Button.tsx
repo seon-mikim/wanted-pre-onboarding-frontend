f876907289faf9a39457ebe82d4bab9976d73123
seon-mikim <tjsalsun@gmail.com>
2023-06-10 21:27:06 +0900

    design: 인라인 스타일 적용
    - 스타일 컨포넌트 적용 전 인라인 스타일 적용

--- src/components/common/button/Button.tsx
+++ src/components/common/button/Button.tsx
@@ -3,14 +3,22 @@ import React from 'react'
 interface ButtonProps {
   title: string
   clickHandle?: (title: string) => void
+  OnCancel?: () => void
 }
 
-export default function Button({ title, clickHandle }: ButtonProps) {
-  const substringTitle = title === '제출' ? title : title.substring(1, 7)
+export default function Button({ title, clickHandle, OnCancel }: ButtonProps) {
+  const substringTitle = title === '제출' || title === '취소' ? title : title.substring(1, 7)
 
   return (
-    <button data-testid={substringTitle + '-button'} onClick={() => clickHandle?.(title)}>
+    <div>
+    <button
+      style={{  padding: `${substringTitle === 'signup'|| 'signin'? '20px 50px': '10px 20px' }`, border: 'none'  }}
+      data-testid={substringTitle + '-button'}
+      onClick={() => clickHandle?.(title)}
+    >
       {substringTitle}
     </button>
+    </div>
+      
   )
 }
