6c6eaf9b601cc43834d7104e29f203893eec254d
seon-mikim <tjsalsun@gmail.com>
2023-06-10 21:37:03 +0900

    style: 가독성을 위한 코드 포멧팅

--- src/components/common/input/Input.tsx
+++ src/components/common/input/Input.tsx
@@ -5,18 +5,16 @@ import { User } from '../../../interface/auth'
 
 interface FormProps {
   pathName: string
-  value?:User 
-  todoInput?:string
+  value?: User
+  todoInput?: string
   onEdit?: boolean
-  formHandle?: (e:React.FormEvent<HTMLFormElement>) => void
-  onChangeHandle?: (e:React.ChangeEvent<HTMLInputElement>) => void
+  formHandle?: (e: React.FormEvent<HTMLFormElement>) => void
+  onChangeHandle?: (e: React.ChangeEvent<HTMLInputElement>) => void
   OnCancel?: () => void
 }
 
-export default function Input({formHandle, onChangeHandle,pathName, value, todoInput, onEdit, OnCancel}: FormProps) {
- 
+export default function Input({ formHandle, onChangeHandle, pathName, value, todoInput, onEdit, OnCancel }: FormProps) {
   const location = useLocation()
-
 
   const renderPathName = location.pathname === '/signup' || location.pathname === '/signin'
   return (
@@ -25,20 +23,39 @@ export default function Input({formHandle, onChangeHandle,pathName, value, todoI
         <S.Form onSubmit={formHandle}>
           <div>
             <S.Label htmlFor="email">email</S.Label>
-            <S.Input onChange={onChangeHandle} value={value?.email} data-testid="email-input" type="text" name='email' id="email" />
+            <S.Input
+              onChange={onChangeHandle}
+              value={value?.email}
+              data-testid="email-input"
+              type="text"
+              name="email"
+              id="email"
+            />
           </div>
-          <div style={{marginBottom: '20px'}}>
+          <div style={{ marginBottom: '20px' }}>
             <S.Label htmlFor="password">password</S.Label>
-            <S.Input onChange={onChangeHandle} value={value?.password} data-testid="password-input" type="password" name='password' id="password" />
+            <S.Input
+              onChange={onChangeHandle}
+              value={value?.password}
+              data-testid="password-input"
+              type="password"
+              name="password"
+              id="password"
+            />
           </div>
           <Button title={pathName} />
         </S.Form>
       ) : (
         <S.TodoForm onSubmit={formHandle}>
-          <S.TodoInput onChange={onChangeHandle} value={todoInput} data-testid="new-todo-input" type="text" name='todo' />
-          <Button title={pathName}/>
-          {onEdit&&<Button title='취소' OnCancel={OnCancel}/>
-          }
+          <S.TodoInput
+            onChange={onChangeHandle}
+            value={todoInput}
+            data-testid="new-todo-input"
+            type="text"
+            name="todo"
+          />
+          <Button title={pathName} />
+          {onEdit && <Button title="취소" OnCancel={OnCancel} />}
         </S.TodoForm>
       )}
     </S.FormWrap>
