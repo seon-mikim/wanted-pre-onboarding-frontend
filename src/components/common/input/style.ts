85845b46c2e4cea922275cca7a77dd584971a005
seon-mikim <tjsalsun@gmail.com>
2023-06-10 21:21:45 +0900

    design: 스타일 컨포넌트 생성
    - 스타일 컨포넌트 각 조건에 맞게 생성 (조건부 렌더링 아님)

--- src/components/common/input/style.ts
+++ src/components/common/input/style.ts
@@ -1,20 +1,36 @@
 import { styled } from 'styled-components'
 
 export const FormWrap = styled.div`
+  width: 100%;
+`
+export const Form = styled.form`
   display: flex;
-  form {
-    label {
-      display: block;
-      font-weight: 500;
-      font-size: 20px;
-      margin-top: 10px;
-      margin-bottom: 10px;
-      text-align: start;
-    }
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+`
+export const TodoForm = styled.form`
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  justify-content: center;
+`
 
-    input {
-      padding: 10px 20px;
-      width: 250px;
-    }
-  }
+export const Label = styled.label`
+  display: block;
+  font-weight: 500;
+  font-size: 20px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  text-align: start;
+`
+
+export const Input = styled.input`
+  padding: 10px 20px;
+  width: 250px;
+`
+export const TodoInput = styled.input`
+  padding: 10px 20px;
+  width: 400px;
+  margin-right: 20px;
 `
