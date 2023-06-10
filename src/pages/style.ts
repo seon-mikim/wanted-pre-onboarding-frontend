f7f481501173bbfee6428515a87207268607c69b
seon-mikim <tjsalsun@gmail.com>
2023-06-10 21:31:07 +0900

    design: TodoPage 스타일 컨포넌트 생성

---
+++ src/pages/style.ts
@@ -0,0 +1,11 @@
+import { styled } from 'styled-components'
+
+
+export const TodoWrap = styled.div  `
+  display: flex;
+  width: 70%;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin: 0 auto;
+`
