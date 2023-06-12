8895907248a5f836a9833d541da24e5bef416416
seon-mikim <tjsalsun@gmail.com>
2023-06-12 19:29:06 +0900

    feat: 스타일 컨포넌트 프롭스를 전달하기위해
    - state 생성

--- src/components/common/link/Link.tsx
+++ src/components/common/link/Link.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import{useState} from 'react'
 import { useLocation } from 'react-router-dom'
+import * as S from './style'
 
 interface LinkProps {
   pathName: string
@@ -8,17 +9,19 @@ interface LinkProps {
 
 export default function Link({ pathName, onNavigate }: LinkProps) {
   const location = useLocation()
+  const [isClicked, setIsClicked] = useState<boolean>(false)
 
   const linkComment = location.pathname === '/signin' || location.pathname === '/' 
       ? '아직 회원이 아니시라면 회원가입 해주세요!!'
       : '회원이라면 로그인해주세요!!'
 
   const handleClick = () => {
+    setIsClicked(!isClicked)
     if (location.pathname !== pathName) {
       onNavigate(pathName)
     } else if (pathName === '/') {
       onNavigate('/signup')
     }
   }
-  return <div style={{ marginTop: '20px'}}onClick={handleClick}>{linkComment}</div>
+  return <S.Link style={{ marginTop: '20px'}} isClicked={isClicked}onClick={handleClick}>{linkComment}</S.Link>
 }
